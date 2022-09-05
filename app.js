ad4b946a3695110a7737cf5a6da3f1868a4ad619
RAVI SHANKAR PRATIHAST <75324945+RaviPratihast@users.noreply.github.com>
2022-09-05 14:08:31 +0530

    add js
    completed with the js logic

--- app.js
+++ app.js
@@ -1,26 +1,84 @@
 'use strict';
 
-const progress=document.getElementById('progress');
-const circles=document.querySelectorAll('.circle');
-const Prev=document.getElementById('btn-prev');
-const Next=document.getElementById('btn-next');
+const progress = document.getElementById('progress');
+const circles = document.querySelectorAll('.circle');
+const Prev = document.getElementById('btn-prev');
+const Next = document.getElementById('btn-next');
 
 // console.log(circles);
 // circles.forEach((e)=> console.log(e))
-let currActive=1;
-// console.log(circles.length)
+let currActive = 1;
+console.log(`currActive:${currActive}`);
 
-Next.addEventListener('click',function(){
+Next.addEventListener('click', function () {
     // Next should go upto 4 only and halts there...and as it will always be on 1 , then the count will start at 2.
 
-    if(currActive<circles.length){
-        currActive++;
+    currActive++;
+    if (currActive > circles.length) {
+        currActive = circles.length;
     }
-   
-    console.log(currActive);
+
+    //   console.log(currActive);
+
+    updateTheProgress();
 });
 
 
-Prev.addEventListener('click',function(){
-    
+
+//  prev
+
+Prev.addEventListener('click', function () {
+    currActive--;
+
+    if (currActive < 1) {
+        currActive = 1;
+    }
+
+    updateTheProgress();
 })
+
+
+
+// actual work in the progress;
+
+function updateTheProgress(e) {
+    circles.forEach((circle, index) => {
+        // console.log(`index:${index} and currActive:${currActive}`);
+
+        if (index < currActive) {
+
+            // though the index 1,2 3,4 that is it (forEach) has gone through each of the circle in just one click, now if condition will work for index less then 2.
+            // console.log(`index:${index} and currActive:${currActive}`);
+            circle.classList.add('circle-active');
+        } else {
+            circle.classList.remove('circle-active');
+        }
+    });
+
+    const active=document.querySelectorAll('.circle-active');
+    // console.log(`active:${active.length}`);
+    // console.log(`circle length:${circles.length}`);
+    // console.log((active.length-1)/(circles.length-1)*100);
+
+    progress.style.width=`${(active.length-1)/(circles.length-1)*100}%`
+
+
+    disableUnableBtn();
+
+
+
+
+}
+
+
+function disableUnableBtn(){
+    if(currActive===1){
+        Prev.disabled=true;
+    }else if(currActive===circles.length){
+        Next.disabled=true;
+    }else{
+        Prev.disabled=false;
+        Next.disabled=false;
+
+    }
+}
